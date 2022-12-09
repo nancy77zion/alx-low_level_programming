eebec8f35ae1b513bb1904f158417b80a562fbb6
nancy77zion <ebnancy710@gmail.com>
2022-12-08 16:25:27 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - prints the size of various types
  *
@@ -15,4 +14,4 @@ int main(void)
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
-i}
+} 
