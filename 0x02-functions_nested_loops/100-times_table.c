2e49e672d03f845b4c628160a39d3790ebac007e
nancy77zion <ebnancy710@gmail.com>
2022-12-14 21:09:40 -0800

    correct

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -25,14 +25,14 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(product + '0');
-				} else if (product >= 10 && product < 100)
+				} else if (product >=i 10 && product < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((product / 10) + '0');
 					_putchar((product % 10) + '0');
-				} else if (product > 100)
+				} else if (product >= 100)
 				{
 					_putchar(',');
 					_putchar(' ');
