ea8a2c5acc89f6832bc082e74b909991ea4b6721
nancy77zion <ebnancy710@gmail.com>
2022-12-14 18:38:20 -0800

    file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,23 +7,25 @@ void times_table(void)
 {
 	int i;
 	int j;
+	int product;
 
 	for (i = 0; i <= 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			int product = j * i;
+			product = j * i;
 
 			if (j == 0)
 			{
 				_putchar('0');
-			} else if (product < 10 && j != 0)
+			}
+			if (product < 10 && j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(product + '0');
-			} else if (product > 10)
+			} else if (product >= 10)
 			{
 				_putchar(',');
 				_putchar(' ');
