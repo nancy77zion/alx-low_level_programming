49e930cdfa6a3ba51781a6561b42cfe795f28359
nancy77zion <ebnancy710@gmail.com>
2023-01-03 19:36:53 -0800

    added last tasks

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -5,10 +5,11 @@
  * @a: 2d array of chars
  * Return: void
  */
-
 void print_chessboard(char (*a)[8])
 {
 	int n, j;
+
+
 
 	n = 0;
 
@@ -17,11 +18,15 @@ void print_chessboard(char (*a)[8])
 		j = 0;
 
 		while (j < 8)
+
 		{
+			
 			_putchar (a[n][j]);
 
 			j++;
+			
 		}
+		
 		_putchar ('\n'); /*print new line*/
 		n++
 	}
